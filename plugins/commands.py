df08121fc728cca297083a88e254022eadb87245
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-02-05 03:03:25 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -106,6 +106,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        protect_content=True,
         )
                     
 
