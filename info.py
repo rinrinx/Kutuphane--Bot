a2f99aa0cb032f515d5e27fb806f3ab3835fc544
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-02-16 00:06:38 +0300

    Update info.py

--- info.py
+++ info.py
@@ -51,3 +51,4 @@ HEROKU_API_KEY = environ.get('HEROKU_API_KEY', None)
 LOG_STR = "Mevcut Özelleştirilmiş Konfigürasyonlar:-\n"
 LOG_STR += (f"CUSTOM_FILE_CAPTION etkinleştirildi {CUSTOM_FILE_CAPTION}, Dosyalarınız bu özelleştirilmiş başlıkla birlikte gönderilecek.\n" if CUSTOM_FILE_CAPTION else "CUSTOM_FILE_CAPTION bulunamadı, dosyanın varsayılan altyazıları kullanılacak.\n")
 LOG_STR += (f"BROADCAST_AS_COPY etkinleştirildi {BROADCAST_AS_COPY}, Bot yayınları etiketsiz olarak yayınlanacak.\n" if BROADCAST_AS_COPY else "BROADCAST_AS_COPY bulunamadı. Yayınlarınız etiketli olarak gönderilecek.\n")
+LOG_STR += (f"LOG_CHANNEL etkinleştirildi {LOG_CHANNEL}, Yeni kullanıcılar botu başlattığında bildirim alacaksınız.\n" if LOG_CHANNEL else "LOG_CHANNEL bulunamadı. Yeni kullanıcıları göremeyeceksiniz.\n")
