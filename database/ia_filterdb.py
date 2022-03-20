420e45b23ce6f662ac5c14e02e5158c4005de0b2
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-03-20 23:46:49 +0300

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -76,7 +76,7 @@ async def get_search_results(query, file_type=None, max_results=10, offset=0, fi
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_]|[ğüşöçİĞÜŞÖÇ]?)' + query + r'(\b|[\.\+\-_]|[ğüşöçİĞÜŞÖÇ]?)'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_\(\)\[\]]')
     
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
