a5676dc0d7a2835717c764ce464ea7bc08c9f270
hackertyus <88194972+hackertyus@users.noreply.github.com>
2022-04-07 22:31:44 +0300

    Update forcesub.py

--- functions/forcesub.py
+++ functions/forcesub.py
@@ -3,11 +3,9 @@ import logging
 from info import AUTH_CHANNEL, FORCE_TXT, BUTTON_TEXT, LOG_CHANNEL, LOG_TEXT_P
 from pyrogram.errors import FloodWait, UserNotParticipant, ChatAdminRequired
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from database.users_chats_db import db
 logger = logging.getLogger(__name__)
 
 async def handle_force_subscribe(bot, message):
-    user_id = message.from_user.id
     try:
         date = message.date + 120
         invite_link = await bot.create_chat_invite_link(AUTH_CHANNEL, expire_date=date, member_limit=1)
@@ -34,8 +32,6 @@ async def handle_force_subscribe(bot, message):
                 )
             ]
         ]
-        if message.command[1] != "subscribe":
-            btn.append([InlineKeyboardButton(" 🔄 Tekrar deneyin", callback_data=f"checksub#{message.command[1]}")])
         await bot.send_message(
             chat_id=message.from_user.id,
             text=FORCE_TXT,
@@ -52,14 +48,4 @@ async def handle_force_subscribe(bot, message):
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
         )
-        return 400
-    if not await db.is_user_exist(user_id):
-        data = await bot.get_me()
-        BOT_USERNAME = data.username
-        await db.add_user(user_id, message.from_user.first_name)
-        if LOG_CHANNEL:
-            await bot.send_message(LOG_CHANNEL,
-                                   text=LOG_TEXT_P.format(user_id, message.from_user.mention, BOT_USERNAME))
-        else:
-            logging.info(f"#YeniKullanıcı :- Ad : {message.from_user.first_name} ID : {user_id}")
         return 400
